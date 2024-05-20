f65f7f3fe8aa939dd280f0dfd933cc8666316f08
Mariano Grasso <44099102+marianograssoOk@users.noreply.github.com>
2024-05-19 21:04:51 -0300

    add endpoint logic with carter extension workaround

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,19 @@
+using BuildingBlocks.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add servicves to ther container
+builder.Services.AddCarterWithAssemblies(typeof(Program).Assembly);
+builder.Services.AddMediatR(config => 
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 // Configure the http request pipeline
+
+app.MapCarter();
 
 app.Run();
