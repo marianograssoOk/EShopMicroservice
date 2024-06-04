3f9f8602424bf41008791788b54948503d9cf019
Mariano Grasso <44099102+marianograssoOk@users.noreply.github.com>
2024-06-04 14:27:28 -0300

    add get by id and get all products

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(
         string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
@@ -9,7 +6,8 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductResult(Guid Id);
 
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -26,7 +24,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price,
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
