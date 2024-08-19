133709b0edbe5a50fda46420c71936be86ca4a86
Mariano Grasso <44099102+marianograssoOk@users.noreply.github.com>
2024-08-19 16:58:24 -0300

    configure docker compose file with basket and order api with rabbitmq

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,15 +1,18 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 public class OrderCreatedEventHandler
-    (IPublishEndpoint pushiPublishEndpoint, ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
 {
-    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
-
-        return Task.CompletedTask;
+        if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -9,7 +9,7 @@ public class OrderName
     public static OrderName Of(string value)
     {
         ArgumentNullException.ThrowIfNull(value);
-        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
 
         return new OrderName(value);
     }
